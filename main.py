8d79feb1852ded6a4b94ae6f7c56cdf70484ea0e
Adib Legastelois <adiblegastelois@MacBook-Air-de-Adib.local>
2021-02-15 16:07:26 +0100

    Show the first and Last element in pizzas's List

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 
 def show_pizza(collection):
     number_of_pizzas = len(collection)
-    print(f"There's actually ({number_of_pizzas}) Pizzas in the List: ")
-    print()
-    for i in collection:
-        print(i)
-    # return collection
+    if number_of_pizzas == 0:
+        print("Sorry there's nothing yet")
+    else:
+        print(f"There's actually ({number_of_pizzas}) Pizzas in the List: ")
+        for i in collection:
+            print(i)
+        print()
+
+        print(f"First one: {collection[0]}")
+        print(f"Last one: {collection[-1]}")
+
+        """" Split to show First and Last Elements in List
+        for i in collection[0::number_of_pizzas-1]:
+            print(i)"""
 
 
 pizzas = ("BlowMinder", "NightStalker", "Fairyzzas", "OnceUponAtamscotta")
